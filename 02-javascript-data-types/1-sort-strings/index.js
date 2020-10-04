592c4429324d104302672985f91302a5d958c321
Anastasia Vasilieva <32935561+aam-fd@users.noreply.github.com>
2020-10-04 20:47:20 +0300

    module 2 task 1 sort function

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,13 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  let sortedArr = arr.slice(0);
+  const collator = new Intl.Collator('kf', { caseFirst: 'upper'});
+  const sortFunction = (a, b) => collator.compare(a, b);
+  switch(param) {
+    case 'asc':
+      return sortedArr.sort(sortFunction);
+    case 'desc':
+      return sortedArr.sort(sortFunction).reverse();
+  }
 }
