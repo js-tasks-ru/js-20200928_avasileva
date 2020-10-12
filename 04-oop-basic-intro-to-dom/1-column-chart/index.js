eb0a761bf7ea921dcaff1fc236ee9f98152df8c6
Anastasia Vasilieva <32935561+aam-fd@users.noreply.github.com>
2020-10-12 22:17:12 +0300

    ColumnChart class

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,89 @@
 export default class ColumnChart {
+  subElements = {};
+  CHAR_HEIGHT = 50;
 
+  constructor({
+    data = [],
+    label = '',
+    value = '',
+    link = 0
+  } = {}) {
+    this.data = data;
+    this.label = label;
+    this.value = value;
+    this.link = link;
+
+    this.render();
+  }
+
+  getColumnBody(data) {
+    const maxValue = Math.max(...data);
+    const scale = this.CHAR_HEIGHT / maxValue;
+
+    return this.data
+      .map(item => {
+        const percent = (item / maxValue * 100).toFixed(0);
+        return `<div style="--value: ${Math.floor(item * scale)}" data-tooltip="${percent}%"></div>`;
+      }).join('');
+  }
+
+  getLink() {
+    return this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : '';
+  }
+
+  getChartLoadingClass(data) {
+    if (data.length === 0) return `column-chart_loading`;
+  }
+
+  get template() {
+    return `
+      <div class="column-chart ${this.getChartLoadingClass(this.data)}" style="---chart-height: ${this.CHAR_HEIGHT}">
+        <div class="column-chart__title">
+          Total ${this.label}
+          ${this.getLink()}
+        </div>
+        <div class="column-chart__container">
+          <div data-element="header" class="column-chart__header">
+            ${this.value}
+          </div>
+          <div data-element="body" class="column-chart__chart">
+            ${this.getColumnBody(this.data)}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    const element = document.createElement('div');
+
+    element.innerHTML = this.template;
+    this.element = element.firstElementChild;
+
+    this.subElements = this.getSubElements(this.element);
+  }
+
+  getSubElements(element) {
+    const elements = element.querySelectorAll('[data-element]');
+
+    return [...elements].reduce((accum, subElement) => {
+      accum[subElement.dataset.element] = subElement;
+
+      return accum;
+    }, {});
+  }
+
+  update(data) {
+    this.subElements.body.innerHTML = this.getColumnBody(data);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    this.subElements = {};
+  }
 }
